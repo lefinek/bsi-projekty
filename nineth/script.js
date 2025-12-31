63e80c6f7d66bfe05f8179c2c7ec728f7d2102bc
Jakub <122076576+lefinek@users.noreply.github.com>
2025-12-31 17:20:54 +0100

    s

--- nineth/script.js
+++ nineth/script.js
@@ -121,10 +121,6 @@ function writeDataToFile(data) {
 }
 
 function fetchSGGWEventsData(url, lastPage) {
-    if (lastPage < 1) {
-        console.error('Invalid number of pages to fetch:', lastPage);
-    }
-    lastPage = 93;
     console.log('Fetching data for', lastPage, 'pages.');
 
     clearDataFile().then(() => {
